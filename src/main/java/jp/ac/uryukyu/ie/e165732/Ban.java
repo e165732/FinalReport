7ae6f2f214dfca42ed3ec42ab3f6dca40a746391
mitsuki <e165732@ie.u-ryukyu.ac.jp>
2017-02-06 12:42:34 +0900

    レポート７ 細かいところを修正。 関数の名前を適切にした。 jarファイルの作成

--- src/main/java/jp/ac/uryukyu/ie/e165732/Ban.java
+++ src/main/java/jp/ac/uryukyu/ie/e165732/Ban.java
@@ -1,19 +1,17 @@
 package jp.ac.uryukyu.ie.e165732;
 
-
 import java.util.Scanner;
-public class Ban {
+
+public class Ban { //"#"を9つ用意し、○または×を代入するクラス。
     String [] ban = new String[9];
-    boolean end = false;//0=false
-    public void print(){
+    boolean end = false;
+    public void print(){ //"#"を9つ用意する。
         for(int i=0;i<=8;i++) {
             ban[i] ="#";
         }
         print2();
         for (int i3=0;i3<=8;){
-            //System.out.printf("%s\n",i3);
-            jugde();
-            //System.out.printf("end=%s\n",end);
+            judge();
             if (end == true){
                 break;
             }else {
@@ -23,19 +21,17 @@ public class Ban {
             if(i3 >=8){
                 break;
             }else {
-                //System.out.printf("%s\n", i3);
-                jugde();
-                //System.out.printf("end=%s\n",end);
+                judge();
                 if (end == true){
                     break;
                 }else {
                     x_turn();
                     i3++;
                 }
-            }jugde();
+            }
         }
     }
-    public void o_turn(){
+    public void o_turn(){ //"#"にoを代入する。
         System.out.printf("Oのターン\n");
         String input;
         Scanner in = new Scanner(System.in);
@@ -44,7 +40,7 @@ public class Ban {
         ban[c] = "o";
         print2();
     }
-    public void x_turn(){
+    public void x_turn(){ //"#"にxを代入する。
         System.out.printf("Xのターン\n");
         String input;
         Scanner in = new Scanner(System.in);
@@ -54,46 +50,36 @@ public class Ban {
         print2();
     }
 
-    public void print2(){
+    public void print2(){ //3×3の格子に"#"が出力するようにした。
         System.out.printf("%s%s%s,0,1,2,\n",ban[0],ban[1],ban[2]);
         System.out.printf("%s%s%s,3,4,5,\n",ban[3],ban[4],ban[5]);
         System.out.printf("%s%s%s,6,7,8,\n",ban[6],ban[7],ban[8]);
     }
-    public void jugde() {
-        //if (ban[0] != "#" && ban[1] != "#" && ban[2] != "#" && ban[3] != "#" && ban[4] != "#" && ban[5] != "#" && ban[6] != "#" && ban[7] != "#" && ban[8] != "#") {
+    public void judge() { //縦横斜め1列が揃ったときに「○または×WIN」と出力するようにした。
         if (ban[0] == ban[3] && ban[3] == ban[6] && ban[0] != "#" &&ban[3] != "#" &&ban[6] != "#" ) {
             System.out.printf("%sWIN\n", ban[0]);
-            end = true;
-        }
-        else if (ban[0] == ban[1] && ban[1] == ban[2]&&ban[0] != "#" &&ban[1] != "#" &&ban[2] != "#" ) {
-            System.out.printf("%sWIN\n", ban[0]);
-            end = true;
-        }
-        else if (ban[0] == ban[4] && ban[4] == ban[8]&&ban[0] != "#" &&ban[4] != "#" &&ban[8] != "#" ) {
-            System.out.printf("%sWIN\n", ban[0]);
-            end = true;
-        }
-        else if (ban[2] == ban[5] && ban[5] == ban[8]&&ban[2] != "#" &&ban[5] != "#" &&ban[8] != "#" ) {
-            System.out.printf("%sWIN\n", ban[2]);
-            end = true;
-        }
-        else if (ban[6] == ban[7] && ban[7] == ban[8]&&ban[6] != "#" &&ban[7] != "#" &&ban[8] != "#" ) {
-            System.out.printf("%sWIN\n", ban[6]);
             end = true;
         } else if (ban[1] == ban[4] && ban[4] == ban[7]&&ban[1] != "#" &&ban[4] != "#" &&ban[7] != "#" ) {
             System.out.printf("%sWIN\n", ban[1]);
             end = true;
+        } else if (ban[2] == ban[5] && ban[5] == ban[8]&&ban[2] != "#" &&ban[5] != "#" &&ban[8] != "#" ) {
+            System.out.printf("%sWIN\n", ban[2]);
+            end = true;
+        } else if (ban[0] == ban[1] && ban[1] == ban[2]&&ban[0] != "#" &&ban[1] != "#" &&ban[2] != "#" ) {
+            System.out.printf("%sWIN\n", ban[0]);
+            end = true;
         } else if (ban[3] == ban[4] && ban[4] == ban[5]&&ban[3] != "#" &&ban[4] != "#" &&ban[5] != "#" ) {
             System.out.printf("%sWIN\n", ban[3]);
+            end = true;
+        } else if (ban[6] == ban[7] && ban[7] == ban[8]&&ban[6] != "#" &&ban[7] != "#" &&ban[8] != "#" ) {
+            System.out.printf("%sWIN\n", ban[6]);
+            end = true;
+        } else if (ban[0] == ban[4] && ban[4] == ban[8]&&ban[0] != "#" &&ban[4] != "#" &&ban[8] != "#" ) {
+            System.out.printf("%sWIN\n", ban[0]);
             end = true;
         } else if (ban[2] == ban[4] && ban[4] == ban[6]&&ban[2] != "#" &&ban[4] != "#" &&ban[6] != "#" ) {
             System.out.printf("%sWIN\n", ban[2]);
             end = true;
-        } else{
-            if (ban[0] != "#" && ban[1] != "#" && ban[2] != "#" && ban[3] != "#" && ban[4] != "#" && ban[5] != "#" && ban[6] != "#" && ban[7] != "#" && ban[8] != "#") {
-                System.out.printf("引き分け");
-            }
         }
     }
-
 }
--- src/main/java/jp/ac/uryukyu/ie/e165732/Main.java
+++ src/main/java/jp/ac/uryukyu/ie/e165732/Main.java
@@ -2,7 +2,7 @@ package jp.ac.uryukyu.ie.e165732;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //mainメソッドでBanクラスを呼び出す。
         Ban p = new Ban();
         p.print();
         System.out.printf("終了");
